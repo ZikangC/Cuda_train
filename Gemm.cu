0b53030242a6b6024537b1f8b3535ee9da9ad894
Zikang Chen <136550709+ZikangC@users.noreply.github.com>
2025-08-19 17:37:10 +0800

    Update Gemm.cu

--- Gemm.cu
+++ Gemm.cu
@@ -1,20 +1,23 @@
-__global__ void gmme(float* A, float* B, float* C, int M,int  N){
+#define blocksize  32
+__global__ void gmme(float* A, float* B, float* C, int N){
     int col = blockDim.x * blockIdx.x + threadIdx.x;
     int row = blockDim.y * blockIdx.y + threadIdx.y;
     int idx = threadIdx.x;
     int idy = threadIdx.y;
-    int once = blockDim.x * blockDim.y * gridDim.x * gridDim.y
-    int cnt = M * N / once;
-    __shared__ float blockdataA[256];
-    __shared__ float blockdataB[256];
-    while(cnt--){
-        blockdataA[idy * N + idx] = A[row * N + col];
-        blockdataB[idy * N + idx] = B[row * N + col];
+    const int n = blocksize;
+    __shared__ float blockdataA[n*n];
+    __shared__ float blockdataB[n*n];
+    float sum = 0.0;
+    for(int i = 0; i < N/n; i++){
+        blockdataA[idy*n + idx] = A[row*N + i*n + idx];
+        blockdataB[idy*n + idx] = B[(idy + i*n) * N + col];
         __syncthreads();
-        for(int i = 0; i < N; i++){
-            C[idy * N + idx] += blockdataA[idy * N + i] * B[i * N + idx];
+        for(int j = 0; j < n; j++){
+            sum += blockdataA[idy * n + j] * blockdataB[j * n + idx];
         }
+        __syncthreads();
+        
     }
-    __syncthreads();
+    C[col*N + row] = sum;
     return;
 }
